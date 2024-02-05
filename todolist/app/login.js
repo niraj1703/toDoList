975d5bc52f03181907933aa259fa0031dc3bc3ce
Niraj Kumar <sharmaniraj763220@gmail.com>
2024-02-05 22:10:15 +0530

    Added login page

---
+++ todolist/app/login.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div>
+        <title>Login page</title>
+        <h1>This is login page</h1>
+    </div>
+  );
+}
